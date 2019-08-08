6eaa8f22758dfa31644d1d789d6b50669ab466bf
Lingyong Sun <lysz210@gmail.com>
2019-08-08 22:24:01 +0200

    init i18n
    create known_locals list
    add cookies translation
    add cps translation

---
+++ i18n/it/cps.ts
@@ -0,0 +1,7 @@
+export default {
+  "titolo": "Controlla il Permesso di Soggiorno",
+  "inSviluppo": "Attenzione il sito è in fase di sviluppo.",
+  "pratica": "Pratica",
+  "verifica": "verifica",
+  "searchHint": "Numero pratica o assicurata"
+}
