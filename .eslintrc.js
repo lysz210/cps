ef6ceef7485b46e9185cb2ebd1e7e368ad9ad6c4
Lingyong Sun <lysz210@gmail.com>
2019-08-13 20:27:34 +0200

    update for eslint for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,15 +5,18 @@ module.exports = {
     node: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: '@typescript-eslint/parser'
   },
   extends: [
     '@nuxtjs',
     'plugin:nuxt/recommended'
   ],
-  plugins: [],
+  plugins: [
+    '@typescript-eslint'
+  ],
   // add your custom rules here
   rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
     'nuxt/no-cjs-in-config': 'off',
     "vue/html-self-closing": 0,
     "no-console": ["error", { "allow": ["log", "error"]}]
