a527503c2d6308dbba9a7037c28bd69645b1d8e5
Lingyong Sun <lysz210@gmail.com>
2019-08-23 17:20:00 +0200

    upgrade store responses
    use guid as id for responses.
    the guid is used to override if exists
    add hide/show response mutatio

--- store/questura/actions.ts
+++ store/questura/actions.ts
@@ -1,5 +1,7 @@
+import { IResponseStatoPratica } from "~/types/lys";
+
 // req must be a Apollo query request
-export async function addResponse ({ commit }, req) {
+export async function addResponse ({ commit }, req: IResponseStatoPratica) {
   /**
    * reset della risposta attuale e visualizza la card
    */
@@ -7,10 +9,10 @@ export async function addResponse ({ commit }, req) {
   commit('setShowCard', true)
   try {
     const res = await req
-    const { data } = res
-    data.show = true
-    commit('addResponse', data)
-    return data
+    const { data: { statoPratica } }= res
+    statoPratica.show = true
+    commit('addResponse', statoPratica)
+    return statoPratica
   } catch (error) {
     console.error('error in Store[questura]', error)
     commit('setErrors', error)
--- store/questura/mutations.ts
+++ store/questura/mutations.ts
@@ -1,3 +1,5 @@
+import { find, merge } from 'lodash'
+// import consola from 'consola'
 export function setQuery (state, q) {
   state.query = q
 }
@@ -6,7 +8,29 @@ export function addResponse (state, response) {
   if (!Array.isArray(state.responses)) {
     state.responses = []
   }
-  state.responses.unshift(response)
+  // consola.info('response', response)
+  let item = find(state.responses, function (element) {
+    return element.item.guid == response.item.guid
+  })
+  if (item) {
+    merge(item, response)
+  } else {
+    state.responses.unshift(response)
+  }
+}
+
+export function hideResponse (state, guid: string) {
+  let item = find(state.responses, element => element.item.guid == guid)
+  if (item) {
+    item.show = false
+  }
+}
+
+export function showResponse (state,  guid: string) {
+  let item = find(state.responses, element => element.item.guid == guid)
+  if (item) {
+    item.show = true
+  }
 }
 
 export function setErrors (state, errors) {
