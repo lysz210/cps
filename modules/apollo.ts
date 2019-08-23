36ca0ac6634890d22ac926b1280493f173de600e
Lingyong Sun <lysz210@gmail.com>
2019-08-23 10:30:05 +0200

    update

--- modules/apollo.ts
+++ modules/apollo.ts
@@ -2,9 +2,9 @@ import { ApolloServer } from 'apollo-server-express'
 import { Module } from '@nuxt/types'
 import consola from 'consola'
 
-import schema from '../database/graphql'
-import { QuesturaApi } from '../my-lib/questura';
 import { get } from 'lodash'
+import schema from '../database/graphql'
+import { QuesturaApi } from '../my-lib/questura'
 
 const apolloModule: Module<any> = function apolloServer (_moduleOptions) {
   const server = new ApolloServer({
@@ -13,7 +13,7 @@ const apolloModule: Module<any> = function apolloServer (_moduleOptions) {
       questuraApi: new QuesturaApi(get(this, 'options.env', {}))
     })
   })
-  consola.log('installing ApolloServer', JSON.stringify(this.env, null, 4))
+  consola.info('installing ApolloServer', JSON.stringify(this.env, null, 4))
   this.nuxt.hook('render:setupMiddleware', (app) => {
     server.applyMiddleware({ app, path: '/gql' })
   })
